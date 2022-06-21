1eac09204fd232e60ffb328258292ebe7934d66a
shubh-cmd <krshubham878@gmail.com>
2022-06-21 13:08:36 +0530

    echo

--- app/bot.sh
+++ app/bot.sh
@@ -2,7 +2,7 @@
 
 for z in $PWD/app/*.zip;do
     dir=$(echo "$z" | awk -F 'app/' {'print $2'} | cut -d '.' -f1)
-    
+    echo $dir
     unzip -o -q "$z" -d $PWD/app/"$dir"
     uagversion=$(cat $PWD/app/"$dir"/version.info | grep unified | awk -F 'gateway-v' {'print $2'})
     
