531be81738fbac7a176246bec250a349eeb28c8a
shubh-cmd <krshubham878@gmail.com>
2022-07-13 02:35:21 +0530

    add public ip in allowed host

--- slack_server/settings.py
+++ slack_server/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if env('ENVIRONMENT') == 'DEV' else False
 
-ALLOWED_HOSTS = ['daphne.herokuapp.com']
+ALLOWED_HOSTS = ['daphne.herokuapp.com', '20.219.109.231']
 
 
 # Application definition
