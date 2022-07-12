cb4267a472d884cd6ff1efc230cc767cf1852f6a
shubh-cmd <krshubham878@gmail.com>
2022-07-13 02:30:49 +0530

    handle error

--- slack_server/urls.py
+++ slack_server/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from slack_bot.views import SlackStartAnalysis, SlackView
+from slack_bot.views import SlackView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
