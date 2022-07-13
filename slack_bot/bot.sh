159768b79c52c7ed96266a1429f771be2879f91f
Ubuntu <selfhelp@SelfHelpSlackBot.wm3c4xh4ydauvecjr1nccvsu3e.tx.internal.cloudapp.net>
2022-07-13 07:14:31 +0000

    make the bot work in production

--- slack_bot/bot.sh
+++ slack_bot/bot.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-
-if [ "$(ls -A ~/Downloads/uag-self-help/log-archive)" ]; then 
+export TERM=xterm
+if [ "$(ls -A /home/selfhelp/projects/uag-self-help/log-archive)" ]; then 
 #    rm -rf /root/uag-self-help/log-archive/*
-    rm -rf ~/Downloads/uag-self-help/log-archive/*
+    rm -rf /home/selfhelp/projects/uag-self-help/log-archive/*
 fi 
 
-cat $PWD/slack_bot/inputs.dat > ~/Downloads/uag-self-help/inputs.dat
-rm ./slack_bot/inputs.dat
-for z in $PWD/slack_bot/*.zip;do
-    cp $z ~/Downloads/uag-self-help/log-archive
+
+echo $(ls /home/selfhelp/projects/uag-bot/slack_bot)
+cat /home/selfhelp/projects/uag-bot/slack_bot/inputs.dat > /home/selfhelp/projects/uag-self-help/inputs.dat
+rm -f /home/selfhelp/projects/uag-bot/slack_bot/inputs.dat
+for z in /home/selfhelp/projects/uag-bot/slack_bot/*.zip;do
+    cp $z /home/selfhelp/projects/uag-self-help/log-archive
     # cp $z /root/uag-self-help/log-archive 
     rm -rf "$z"
 done 
 
-cmd=$(cd ~/Downloads/uag-self-help && bash Analyze.sh)
+cmd=$(cd /home/selfhelp/projects/uag-self-help && bash Analyze.sh)
+#echo $(ls /home/selfhelp/projects/uag-self-help/analysis-logs)
+compress=$(cd /home/selfhelp/projects/uag-self-help && zip -rq /home/selfhelp/projects/uag-bot/slack_bot/analysis-logs.zip analysis-logs)
 
-compress=$(cd ~/Downloads/uag-self-help && zip -rq /home/shubham/Desktop/projects/server/slack_server/slack_bot/analysis-logs.zip analysis-logs)
+curl -s -F file=@"/home/selfhelp/projects/uag-bot/slack_bot/analysis-logs.zip" -F "initial_comment=Result of the analysis" -F channels="C03L2T10R43" -H "Authorization: Bearer $1" https://slack.com/api/files.upload -o /dev/null
 
-curl -s -F file=@"/home/shubham/Desktop/projects/server/slack_server/slack_bot/analysis-logs.zip" -F "initial_comment=Result of the analysis" -F channels="C03L2T10R43" -H "Authorization: Bearer $1" https://slack.com/api/files.upload -o /dev/null
+rm -rf /home/selfhelp/projects/uag-bot/slack_bot/analysis-logs.zip
 
-rm -rf ./slack_bot/analysis-logs.zip
+echo $(ls /home/selfhelp/projects/uag-bot/slack_bot)
